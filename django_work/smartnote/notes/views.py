c49e34d1d6e590d6edab62e3f981fd74af8fa9c8
Dharm Vashisth <dharmvashisth5@gmail.com>
2025-06-08 19:02:02 +0530

    dynamic template.

--- django_work/smartnote/notes/views.py
+++ django_work/smartnote/notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
+def list_notes(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
