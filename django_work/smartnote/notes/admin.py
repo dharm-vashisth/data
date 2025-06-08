806f8d0edda499478a471d4e5a60ad5c9586c397
Dharm Vashisth <dharmvashisth5@gmail.com>
2025-06-08 10:29:31 +0530

    Entry in the model and denoting using title on admin panel.

--- django_work/smartnote/notes/admin.py
+++ django_work/smartnote/notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ['title']  # display the title per record in the django admin panel
+
+admin.site.register(models.Notes, NotesAdmin)
