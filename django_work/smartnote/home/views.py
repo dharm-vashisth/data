5f0c5e10326d944b8af3777dd22f01f9ac614038
Dharm Vashisth <dharmvashisth5@gmail.com>
2025-06-08 10:08:33 +0530

    login required access control.

--- django_work/smartnote/home/views.py
+++ django_work/smartnote/home/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html',{'date_': datetime.today()})  # passing the parameter
+
+@login_required(login_url='/admin')
+def login(request):
+    return render(request, 'home/authorized_page.html',{})
