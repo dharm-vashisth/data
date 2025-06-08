7494956696d82ee5489a229401057cd021a79ec9
Dharm Vashisth <dharmvashisth5@gmail.com>
2025-06-08 08:56:30 +0530

    passing value from views to template.

--- django_work/smartnote/home/views.py
+++ django_work/smartnote/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html',{})
+    return render(request, 'home/index.html',{'date_': datetime.today()})  # passing the parameter
