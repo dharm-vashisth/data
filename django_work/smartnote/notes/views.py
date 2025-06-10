e4747d9085599458475d968e87878f9ffd6767b6
Dharm Vashisth <dharmvashisth5@gmail.com>
2025-06-10 10:32:40 +0530

    handling 404 page for unexpected request.

--- django_work/smartnote/notes/views.py
+++ django_work/smartnote/notes/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Notes
+from django.http import Http404
 
-# Create your views here.
+
+# view for list of notes.
 def list_notes(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
+
+# view for individual note.
 def note_details(request, pk):
-    note = Notes.objects.get(pk=pk)
-    return render(request,'notes/note_details.html',{'note':note})
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exists.")
+
+    return render(request, 'notes/note_details.html', {'note': note})
